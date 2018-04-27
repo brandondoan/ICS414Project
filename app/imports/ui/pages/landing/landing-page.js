ca3bd63a3ec6ae981b94a3f521bf9a87d0a2a160
brandondoan <doanb@hawaii.edu>
2018-04-27 04:54:47 -1000

    landing page 2
    Updated the second landing page.
    Now when you click on a cause, it will update the alert type value for the alert to the cause, afterwards it routes the user to the send option page.

--- app/imports/ui/pages/landing/landing-page.js
+++ app/imports/ui/pages/landing/landing-page.js
@@ -7,17 +7,17 @@ Template.Landing_Page.onCreated(function onCreated() {
 });
 
 Template.Landing_Page.events({
-  'mousedown button': function (event) {
-    const test = event.target.value;
-    const testBool = (test === 'true'); // this is just to make it a boolean and not a string
-    const newAlert = { // we make an object thingy to insert into db
-      alertType: '',
-      area: ['s', 'nek'],
-      sendMethod: ['s', 'nek'],
-      test: testBool,
-    };
-    const id = Alerts._collection.insert(newAlert); // i have no idea why this works
-    console.log(id);
-    FlowRouter.go(`/${id}/cause`); // route to specific page for db items
-  },
+    'mousedown button': function (event) {
+        const test = event.target.value;
+        const testBool = (test === 'true'); // this is just to make it a boolean and not a string
+        const newAlert = { // we make an object thingy to insert into db
+            alertType: '',
+            area: ['s', 'nek'],
+            sendMethod: ['s', 'nek'],
+            test: testBool,
+        };
+        const id = Alerts._collection.insert(newAlert); // i have no idea why this works
+        console.log(id);
+        FlowRouter.go(`/${id}/cause`); // route to specific page for db items
+    },
 });
--- app/imports/ui/pages/landing2/landing-page2.js
+++ app/imports/ui/pages/landing2/landing-page2.js
@@ -16,12 +16,54 @@ function addAlert(alert) {
 }
 
 Template.Landing_Page2.events({
-  'mousedown button': function (event) {
-    addAlert('a');
-  },
-  'mousedown #ui #raised #segment': function (event) {
+
+  'mousedown #surf': function (event) {
+      addAlert('surf');
+      FlowRouter.go(`/alert`);
     console.log(event.target);
   },
+
+    'mousedown #landslide': function (event) {
+        addAlert('landslide');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
+
+    'mousedown #flood': function (event) {
+        addAlert('flood');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
+
+    'mousedown #earthquake': function (event) {
+        addAlert('earthquake');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
+
+    'mousedown #tsunami': function (event) {
+        addAlert('tsunami');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
+
+    'mousedown #missile': function (event) {
+        addAlert('missile');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
+
+    'mousedown #amber': function (event) {
+        addAlert('amber');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
+
+    'mousedown #hurricane': function (event) {
+        addAlert('hurricane');
+        FlowRouter.go(`/alert`);
+        console.log(event.target);
+    },
 });
 
 Meteor.call('CellAlert', 'al', 'ert');
