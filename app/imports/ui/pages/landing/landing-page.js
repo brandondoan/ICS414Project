1203dd6d507f8f5f7be34ff26bfc6a2b5c84cb95
tylergdorn <31430148+tylergdorn@users.noreply.github.com>
2018-04-27 18:50:19 -1000

    git hub does weird things sorry

--- app/imports/ui/pages/landing/landing-page.js
+++ app/imports/ui/pages/landing/landing-page.js
@@ -3,12 +3,10 @@ import { Alerts } from '/imports/api/alert/AlertCollection';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 Template.Landing_Page.onCreated(function onCreated() {
-  this.subscribe(Alerts.getPublicationName());
     this.subscribe(Alerts.getPublicationName());
 });
 
 Template.Landing_Page.events({
-<<<<<<< HEAD
   'mousedown button': function (event) {
     const test = event.target.value;
     const testBool = (test === 'true'); // this is just to make it a boolean and not a string
@@ -17,12 +15,9 @@ Template.Landing_Page.events({
       area: [],
       sendMethod: [],
       test: testBool,
-=======
->>>>>>> 185345f0fabe49e8311da08468b7f31a7745ce40
     };
     const id = Alerts._collection.insert(newAlert); // i have no idea why this works
     FlowRouter.go(`/${id}/cause`); // route to specific page for db items
   },
- },
 
 });
